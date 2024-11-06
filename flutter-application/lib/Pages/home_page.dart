138048d00c5c1d2e33143c2e1c05b4211e6fbaf5
Eddie-Pyn <146141186+Eddie-Pyn@users.noreply.github.com>
2024-11-06 17:13:07 -0500

    QR Code Implementation

--- flutter-application/lib/Pages/home_page.dart
+++ flutter-application/lib/Pages/home_page.dart
@@ -1,19 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:my_navigation_app/qrscan.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   final double textSize;
-  const HomePage({super.key, required this.textSize});
+  const HomePage({Key? key, required this.textSize});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page - Empty!',
-        style: TextStyle(
-            fontFamily: 'Sans-serif',
-            fontSize: textSize,
-            color: const Color(0xFF00A94F)),
-      ),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
+class _HomePageState extends State<HomePage>{
+  final TextEditingController _result = TextEditingController();
+  
+  void setresult(String result){
+    _result.text = result;
+  }
+
+@override
+Widget build(BuildContext context) {
+  return Center(
+    child: SizedBox(
+      width: 250,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [TextField(
+          controller: _result,
+            decoration: InputDecoration(
+              labelText: 'Current location',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Enter a Location',
+            ),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => QrCodeScanner(setresult: setresult),
+              ),
+            ),
+            child: Text("Scan QR"),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+
+}
+
---
+++ flutter-application/lib/qrscan.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class QrCodeScanner extends StatelessWidget {
+  QrCodeScanner({
+    required this.setresult,
+    super.key,
+    });
+
+  final Function setresult;
+  final MobileScannerController controller = MobileScannerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MobileScanner(
+      controller: controller,
+      onDetect: (BarcodeCapture capture) async {
+        final List<Barcode> barcodes = capture.barcodes;
+        final barcode = barcodes.first;
+
+        if(barcode.rawValue != null){
+          setresult(barcode.rawValue);
+          await controller
+            .stop()
+            .then((value) => controller.dispose())
+            .then((value) => Navigator.of(context).pop());
+        }
+      },
+    );
+  }
+}
