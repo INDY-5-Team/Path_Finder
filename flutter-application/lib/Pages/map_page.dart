ddb8865c531de489ba695e326ec3d1c54f5e1f5a
Harrison Varnadoe <harrison.varnadoe@outlook.com>
2024-11-13 18:22:06 -0500

    started adding nodes

--- flutter-application/lib/Pages/map_page.dart
+++ flutter-application/lib/Pages/map_page.dart
@@ -16,15 +16,102 @@ class MapPage extends StatelessWidget {
         minScale: 0.5,
         maxScale: 4.0,
         child: Center(
-          child: SizedBox(
-            width: screenSize.width * 2,
-            height: screenSize.height * 2,
-            child: SvgPicture.asset(
-              'assets/images/mock_app_no_bg.svg',
-              fit: BoxFit.none,
+          child: Stack(
+            children: [
+              SizedBox(
+              width: screenSize.width * 2,
+              height: screenSize.height * 2,
+              child: SvgPicture.asset(
+                'assets/images/mock_app_no_bg.svg',
+                fit: BoxFit.none,
             ),
           ),
-        ),
+          Positioned(
+            top: 40, left: 180,
+            key: Key('J-135'),
+            child: Row(
+                children: <Widget>[                  
+                  Text(
+                    "J-135",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,                      
+                    )),
+                  Icon(Icons.location_pin, color: Colors.blue),
+                ]),                          
+          ),
+          Positioned(
+            top: 80, left: 180,
+            key: Key('J-133'),
+            child: Row(
+                children: <Widget>[                  
+                  Text(
+                    "J-133",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,                      
+                    )),
+                  Icon(Icons.location_pin, color: Colors.blue),
+                ]),                          
+          ),
+          Positioned(
+            top: 120, left: 180,
+            key: Key('J-131'),
+            child: Row(
+                children: <Widget>[                  
+                  Text(
+                    "J-131",
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,                      
+                    )),
+                  Icon(Icons.location_pin, color: Colors.blue),
+                ]),                          
+          ),
+          Positioned(
+            top: 168, left: 180,
+            key: Key('J-120'),
+            child: Row(
+                children: <Widget>[                  
+                  Text(
+                    "J-120",
+                    style: TextStyle(
+                      fontSize: 11,
+                      color: Colors.black,                      
+                    )),
+                  Icon(Icons.location_pin, color: Colors.blue),
+                ]),                          
+          ),
+          Positioned(
+            top: 216, left: 180,
+            key: Key('J-109'),
+            child: Row(
+                children: <Widget>[                  
+                  Text(
+                    "J-109",
+                    style: TextStyle(
+                      fontSize: 11,
+                      color: Colors.black,                      
+                    )),
+                  Icon(Icons.location_pin, color: Colors.blue),
+                ]),                          
+          ),
+          Positioned(
+            top: 260, left: 180,
+            key: Key('J-107'),
+            child: Row(
+                children: <Widget>[                  
+                  Text(
+                    "J-107",
+                    style: TextStyle(
+                      fontSize: 11,
+                      color: Colors.black,                      
+                    )),
+                  Icon(Icons.location_pin, color: Colors.blue),
+                ]),                          
+          )
+        ]),
+        )
       ),
     );
   }
