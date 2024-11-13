c1ca1cbb19f25cd66c01b6e519845466b4366b29
Harrison Varnadoe <harrison.varnadoe@outlook.com>
2024-11-13 00:28:46 -0500

    added graph and algorithm

--- flutter-application/lib/Pages/home_page.dart
+++ flutter-application/lib/Pages/home_page.dart
@@ -1,54 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:my_navigation_app/qrscan.dart';
+import 'package:my_navigation_app/Utilities/qrscan.dart';
 
 class HomePage extends StatefulWidget {
   final double textSize;
-  const HomePage({Key? key, required this.textSize});
+  const HomePage({super.key, required this.textSize});
 
   @override
-  _HomePageState createState() => _HomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
-class _HomePageState extends State<HomePage>{
+
+class _HomePageState extends State<HomePage> {
   final TextEditingController _result = TextEditingController();
-  
-  void setresult(String result){
+
+  void setresult(String result) {
     _result.text = result;
   }
 
-@override
-Widget build(BuildContext context) {
-  return Center(
-    child: SizedBox(
-      width: 250,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [TextField(
-          controller: _result,
-            decoration: InputDecoration(
-              labelText: 'Current location',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          const TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Enter a Location',
-            ),
-          ),
-          TextButton(
-            onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => QrCodeScanner(setresult: setresult),
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _result,
+              decoration: InputDecoration(
+                labelText: 'Current location',
+                border: OutlineInputBorder(),
               ),
             ),
-            child: Text("Scan QR"),
-          ),
-        ],
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter a Location',
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => QrCodeScanner(setresult: setresult),
+                ),
+              ),
+              child: Text("Scan QR"),
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
-
-}
-
