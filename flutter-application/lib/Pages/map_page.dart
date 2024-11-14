e534e5323fd003ffdf3da0b30d38e9ac594a8b6f
Harrison Varnadoe <harrison.varnadoe@outlook.com>
2024-11-13 23:44:46 -0500

    stuff

--- flutter-application/lib/Pages/map_page.dart
+++ flutter-application/lib/Pages/map_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+import 'package:my_navigation_app/Utilities/map.dart';
 
 class MapPage extends StatelessWidget {
   final double textSize;
@@ -12,7 +12,12 @@ class MapPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final pinColor = Colors.blue;
     final hallwayColor = Colors.amber;
-
+    List<String> route = [];
+    if(currentLocation != null && destination != null){
+      Graph map = Graph();
+      map.loadFromJson('assets/maps/atrium_f1.json');
+      route = map.route(currentLocation!, destination!);
+    }
     return Scaffold(
       body: InteractiveViewer(
         panEnabled: true,
@@ -63,47 +68,47 @@ class MapPage extends StatelessWidget {
                   buildRoom(screenWidth, screenHeight, 0.1, 0.57, "J-132", pinColor),
                   buildRoom(screenWidth, screenHeight, 0.05, 0.57, "J-134", pinColor),
 
-                  buildHall(screenWidth, screenHeight, 0.05, 0.53, 'H1', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.1, 0.53, 'H2', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.12, 0.53, 'H3', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.14, 0.53, 'H4', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.17, 0.53, 'H5', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.19, 0.53, 'H6', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.21, 0.53, 'H7', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.25, 0.53, 'H8', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.28, 0.53, 'H9', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.30, 0.53, 'H10', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.33, 0.53, 'H11', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.36, 0.53, 'H12', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.37, 0.53, 'H13', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.37, 0.53, 'H14', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.53, 'H15', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.58, 'H16', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.63, 'H17', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.66, 'H18', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.49, 0.66, 'H19', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.545, 0.66, 'H20', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.545, 0.62, 'H21', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.545, 0.60, 'H22', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.545, 0.53, 'H23', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.57, 0.53, 'H24', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.585, 0.53, 'H25', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.61, 0.53, 'H26', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.625, 0.53, 'H27', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.67, 0.53, 'H28', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.69, 0.53, 'H29', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.70, 0.53, 'H30', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.71, 0.53, 'H31', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.75, 0.53, 'H32', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.82, 0.53, 'H33', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.835, 0.53, 'H34', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.87, 0.53, 'H35', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.88, 0.53, 'H36', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.93, 0.53, 'H37', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.95, 0.53, 'H38', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.545, 0.42, 'H39', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.49, 0.42, 'H40', hallwayColor),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.42, 'H41', hallwayColor),
+                  buildHall(screenWidth, screenHeight, 0.05, 0.53, 'H1', hallwayColor, route.contains('H1')),
+                  buildHall(screenWidth, screenHeight, 0.1, 0.53, 'H2', hallwayColor, route.contains('H2')),
+                  buildHall(screenWidth, screenHeight, 0.12, 0.53, 'H3', hallwayColor, route.contains('H3')),
+                  buildHall(screenWidth, screenHeight, 0.14, 0.53, 'H4', hallwayColor, route.contains('H4')),
+                  buildHall(screenWidth, screenHeight, 0.17, 0.53, 'H5', hallwayColor, route.contains('H5')),
+                  buildHall(screenWidth, screenHeight, 0.19, 0.53, 'H6', hallwayColor, route.contains('H6')),
+                  buildHall(screenWidth, screenHeight, 0.21, 0.53, 'H7', hallwayColor, route.contains('H7')),
+                  buildHall(screenWidth, screenHeight, 0.25, 0.53, 'H8', hallwayColor, route.contains('H8')),
+                  buildHall(screenWidth, screenHeight, 0.28, 0.53, 'H9', hallwayColor, route.contains('H9')),
+                  buildHall(screenWidth, screenHeight, 0.30, 0.53, 'H10', hallwayColor, route.contains('H10')),
+                  buildHall(screenWidth, screenHeight, 0.33, 0.53, 'H11', hallwayColor, route.contains('H11')),
+                  buildHall(screenWidth, screenHeight, 0.36, 0.53, 'H12', hallwayColor, route.contains('H12')),
+                  buildHall(screenWidth, screenHeight, 0.37, 0.53, 'H13', hallwayColor, route.contains('H13')),
+                  buildHall(screenWidth, screenHeight, 0.37, 0.53, 'H14', hallwayColor, route.contains('H14')),
+                  buildHall(screenWidth, screenHeight, 0.43, 0.53, 'H15', hallwayColor, route.contains('H15')),
+                  buildHall(screenWidth, screenHeight, 0.43, 0.58, 'H16', hallwayColor, route.contains('H16')),
+                  buildHall(screenWidth, screenHeight, 0.43, 0.63, 'H17', hallwayColor, route.contains('H17')),
+                  buildHall(screenWidth, screenHeight, 0.43, 0.66, 'H18', hallwayColor, route.contains('H18')),
+                  buildHall(screenWidth, screenHeight, 0.49, 0.66, 'H19', hallwayColor, route.contains('H19')),
+                  buildHall(screenWidth, screenHeight, 0.545, 0.66, 'H20', hallwayColor, route.contains('H20')),
+                  buildHall(screenWidth, screenHeight, 0.545, 0.62, 'H21', hallwayColor, route.contains('H21')),
+                  buildHall(screenWidth, screenHeight, 0.545, 0.60, 'H22', hallwayColor, route.contains('H22')),
+                  buildHall(screenWidth, screenHeight, 0.545, 0.53, 'H23', hallwayColor, route.contains('H23')),
+                  buildHall(screenWidth, screenHeight, 0.57, 0.53, 'H24', hallwayColor, route.contains('H24')),
+                  buildHall(screenWidth, screenHeight, 0.585, 0.53, 'H25', hallwayColor, route.contains('H25')),
+                  buildHall(screenWidth, screenHeight, 0.61, 0.53, 'H26', hallwayColor, route.contains('H26')),
+                  buildHall(screenWidth, screenHeight, 0.625, 0.53, 'H27', hallwayColor, route.contains('H27')),
+                  buildHall(screenWidth, screenHeight, 0.67, 0.53, 'H28', hallwayColor, route.contains('H28')),
+                  buildHall(screenWidth, screenHeight, 0.69, 0.53, 'H29', hallwayColor, route.contains('H29')),
+                  buildHall(screenWidth, screenHeight, 0.70, 0.53, 'H30', hallwayColor, route.contains('H30')),
+                  buildHall(screenWidth, screenHeight, 0.71, 0.53, 'H31', hallwayColor, route.contains('H31')),
+                  buildHall(screenWidth, screenHeight, 0.75, 0.53, 'H32', hallwayColor, route.contains('H32')),
+                  buildHall(screenWidth, screenHeight, 0.82, 0.53, 'H33', hallwayColor, route.contains('H33')),
+                  buildHall(screenWidth, screenHeight, 0.835, 0.53, 'H34', hallwayColor, route.contains('H34')),
+                  buildHall(screenWidth, screenHeight, 0.87, 0.53, 'H35', hallwayColor, route.contains('H35')),
+                  buildHall(screenWidth, screenHeight, 0.88, 0.53, 'H36', hallwayColor, route.contains('H36')),
+                  buildHall(screenWidth, screenHeight, 0.93, 0.53, 'H37', hallwayColor, route.contains('H37')),
+                  buildHall(screenWidth, screenHeight, 0.95, 0.53, 'H38', hallwayColor, route.contains('H38')),
+                  buildHall(screenWidth, screenHeight, 0.545, 0.42, 'H39', hallwayColor, route.contains('H39')),
+                  buildHall(screenWidth, screenHeight, 0.49, 0.42, 'H40', hallwayColor, route.contains('H40')),
+                  buildHall(screenWidth, screenHeight, 0.43, 0.42, 'H41', hallwayColor, route.contains('H41')),
                 ],
               );
             },
@@ -113,7 +118,6 @@ class MapPage extends StatelessWidget {
     );
   }
 
-  // Helper function to create positioned text and icon elements based on percentages
   Positioned buildRoom(
       double screenWidth, double screenHeight, double topPercent, double leftPercent, String label, Color color) {
     return Positioned(
@@ -135,17 +139,16 @@ class MapPage extends StatelessWidget {
     );
   }
 
-  // Helper function to create positioned circle icons based on percentages
-  Positioned buildHall(double screenWidth, double screenHeight, double topPercent, double leftPercent, String label, Color color) {
+  Positioned buildHall(double screenWidth, double screenHeight, double topPercent, double leftPercent, String label, Color color, bool visible) {
     return Positioned(
       top: screenHeight * topPercent,
       left: screenWidth * leftPercent,
       key: Key(label),
-      child: Icon(
+      child: visible ? Icon(
         Icons.circle_rounded,
         color: color,
         size: textSize / 3,
-      ),
+      ): SizedBox.shrink(),
     );
   }
 }
