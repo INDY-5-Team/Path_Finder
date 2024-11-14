2d05f50890dbe64ee00484449677aeac3f8ba227
Harrison Varnadoe <harrison.varnadoe@outlook.com>
2024-11-14 01:41:01 -0500

    made it prettier, fixed all node location, and  added ability to search from the map page

--- flutter-application/lib/Pages/map_page.dart
+++ flutter-application/lib/Pages/map_page.dart
@@ -1,72 +1,123 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_navigation_app/Utilities/map.dart';
+import 'package:my_navigation_app/Utilities/qrscan.dart';
 
 class MapPage extends StatelessWidget {
   final double textSize;
   final String? currentLocation;
-  final String? destination;
+  final String? destination;  
   const MapPage({super.key, required this.textSize, this.currentLocation, this.destination});
-
+  
   @override
-  Widget build(BuildContext context) {
-    final pinColor = Colors.blue;
+Widget build(BuildContext context) {
+  final pinColor = Colors.blue;
     final hallwayColor = Colors.amber;
-    List<String> route = [];
+    final TextEditingController currloca = TextEditingController();
+    final TextEditingController dest = TextEditingController();
+
+    List<dynamic> route = [];
     if(currentLocation != null && destination != null){
-      Graph map = Graph();
-      map.loadFromJson('assets/maps/atrium_f1.json');
-      route = map.route(currentLocation!, destination!);
+      Graph graph = Graph();
+      graph.loadFromJson('assets/maps/atrium_f1.json', context);
+      route = graph.route(currentLocation!, destination!);
     }
-    return Scaffold(
-      body: InteractiveViewer(
-        panEnabled: true,
-        boundaryMargin: EdgeInsets.all(100),
-        minScale: 0.5,
-        maxScale: 4.0,
-        child: Center(
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              final screenWidth = constraints.maxWidth;
-              final screenHeight = constraints.maxHeight;
-              return Stack(
-                children: [
-                  SizedBox(
-                    width: screenWidth * 2,
-                    height: screenHeight * 2,
-                    child: SvgPicture.asset(
-                      'assets/images/mock_app_no_bg.svg',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                  buildRoom(screenWidth, screenHeight, 0.05, 0.4, "J-135", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.1, 0.4, "J-133", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.14, 0.4, "J-131", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.201, 0.4, "J-120", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.27, 0.4, "J-109", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.32, 0.4, "J-107", pinColor),
+    void setresult(String result) {
+    currloca.text = result;
+    }
+  return Scaffold(
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            controller: currloca,
+            decoration: InputDecoration(
+              labelText: 'Current Location',
+              border: OutlineInputBorder(),
+              suffixIcon: IconButton(
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => QrCodeScanner(setresult: setresult)),
+                ),
+                icon: Icon(Icons.qr_code),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: dest,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Enter Destination',
+            ),
+          ),
+          const SizedBox(height: 16),
+          FloatingActionButton(
+            onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => MapPage(
+                  textSize: textSize,
+                  currentLocation: currloca.text,
+                  destination: dest.text,
+                ),
+              ),
+            ),
+            backgroundColor: Colors.blue,
+            child: const Icon(Icons.search),
+          ),
+          const SizedBox(height: 16),
+
+          // Wrapping InteractiveViewer in an Expanded widget
+          Expanded(
+            child: InteractiveViewer(
+              panEnabled: true,
+              boundaryMargin: EdgeInsets.all(100),
+              minScale: 0.5,
+              maxScale: 4.0,
+              child: Center(
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final screenWidth = constraints.maxWidth;
+                    final screenHeight = constraints.maxHeight;
+                    return Stack(
+                      children: [
+                        SizedBox(
+                          width: screenWidth * 2,
+                          height: screenHeight * 2,
+                          child: SvgPicture.asset(
+                            'assets/images/mock_app_no_bg.svg',
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        buildRoom(screenWidth, screenHeight, 0.05, 0.40909, "J-135", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.1, 0.409, "J-133", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.14, 0.409, "J-131", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.201, 0.409, "J-120", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.27, 0.409, "J-109", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.32, 0.409, "J-107", pinColor),
                   buildRoom(screenWidth, screenHeight, 0.37, 0.38, "J-105", pinColor),
                   buildRoom(screenWidth, screenHeight, 0.39, 0.3, "Stacks", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.58, 0.3, "J-152", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.58, 0.67, "J-151", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.65, 0.4, "J-156", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.73, 0.4, "J-158", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.82, 0.4, "J-160", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.92, 0.4, "J-163", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.92, 0.58, "J-165", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.85, 0.58, "J-164", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.78, 0.58, "J-161", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.72, 0.58, "J-157", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.69, 0.58, "J-159", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.6, 0.58, "J-153", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.37, 0.68, "J-101", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.35, 0.55, "J-103", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.31, 0.57, "J-106", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.25, 0.57, "J-108", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.20, 0.57, "J-110", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.14, 0.57, "J-130", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.1, 0.57, "J-132", pinColor),
-                  buildRoom(screenWidth, screenHeight, 0.05, 0.57, "J-134", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.58, 0.35, "J-152", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.58, 0.60, "J-151", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.65, 0.409, "J-156", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.73, 0.409, "J-158", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.82, 0.409, "J-160", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.92, 0.409, "J-163", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.92, 0.53, "J-165", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.85, 0.53, "J-164", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.78, 0.53, "J-161", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.70, 0.53, "J-157", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.64, 0.53, "J-159", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.6, 0.55, "J-153", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.37, 0.60, "J-101", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.35, 0.52, "J-103", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.31, 0.53, "J-106", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.25, 0.53, "J-108", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.20, 0.53, "J-110", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.14, 0.53, "J-130", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.1, 0.53, "J-132", pinColor),
+                  buildRoom(screenWidth, screenHeight, 0.05, 0.53, "J-134", pinColor),
 
                   buildHall(screenWidth, screenHeight, 0.05, 0.53, 'H1', hallwayColor, route.contains('H1')),
                   buildHall(screenWidth, screenHeight, 0.1, 0.53, 'H2', hallwayColor, route.contains('H2')),
@@ -82,17 +133,17 @@ class MapPage extends StatelessWidget {
                   buildHall(screenWidth, screenHeight, 0.36, 0.53, 'H12', hallwayColor, route.contains('H12')),
                   buildHall(screenWidth, screenHeight, 0.37, 0.53, 'H13', hallwayColor, route.contains('H13')),
                   buildHall(screenWidth, screenHeight, 0.37, 0.53, 'H14', hallwayColor, route.contains('H14')),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.53, 'H15', hallwayColor, route.contains('H15')),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.58, 'H16', hallwayColor, route.contains('H16')),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.63, 'H17', hallwayColor, route.contains('H17')),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.66, 'H18', hallwayColor, route.contains('H18')),
-                  buildHall(screenWidth, screenHeight, 0.49, 0.66, 'H19', hallwayColor, route.contains('H19')),
+                  buildHall(screenWidth, screenHeight, 0.4093, 0.53, 'H15', hallwayColor, route.contains('H15')),
+                  buildHall(screenWidth, screenHeight, 0.4093, 0.53, 'H16', hallwayColor, route.contains('H16')),
+                  buildHall(screenWidth, screenHeight, 0.4093, 0.63, 'H17', hallwayColor, route.contains('H17')),
+                  buildHall(screenWidth, screenHeight, 0.4093, 0.66, 'H18', hallwayColor, route.contains('H18')),
+                  buildHall(screenWidth, screenHeight, 0.4099, 0.66, 'H19', hallwayColor, route.contains('H19')),
                   buildHall(screenWidth, screenHeight, 0.545, 0.66, 'H20', hallwayColor, route.contains('H20')),
                   buildHall(screenWidth, screenHeight, 0.545, 0.62, 'H21', hallwayColor, route.contains('H21')),
                   buildHall(screenWidth, screenHeight, 0.545, 0.60, 'H22', hallwayColor, route.contains('H22')),
                   buildHall(screenWidth, screenHeight, 0.545, 0.53, 'H23', hallwayColor, route.contains('H23')),
-                  buildHall(screenWidth, screenHeight, 0.57, 0.53, 'H24', hallwayColor, route.contains('H24')),
-                  buildHall(screenWidth, screenHeight, 0.585, 0.53, 'H25', hallwayColor, route.contains('H25')),
+                  buildHall(screenWidth, screenHeight, 0.53, 0.53, 'H24', hallwayColor, route.contains('H24')),
+                  buildHall(screenWidth, screenHeight, 0.535, 0.53, 'H25', hallwayColor, route.contains('H25')),
                   buildHall(screenWidth, screenHeight, 0.61, 0.53, 'H26', hallwayColor, route.contains('H26')),
                   buildHall(screenWidth, screenHeight, 0.625, 0.53, 'H27', hallwayColor, route.contains('H27')),
                   buildHall(screenWidth, screenHeight, 0.67, 0.53, 'H28', hallwayColor, route.contains('H28')),
@@ -106,17 +157,21 @@ class MapPage extends StatelessWidget {
                   buildHall(screenWidth, screenHeight, 0.88, 0.53, 'H36', hallwayColor, route.contains('H36')),
                   buildHall(screenWidth, screenHeight, 0.93, 0.53, 'H37', hallwayColor, route.contains('H37')),
                   buildHall(screenWidth, screenHeight, 0.95, 0.53, 'H38', hallwayColor, route.contains('H38')),
-                  buildHall(screenWidth, screenHeight, 0.545, 0.42, 'H39', hallwayColor, route.contains('H39')),
-                  buildHall(screenWidth, screenHeight, 0.49, 0.42, 'H40', hallwayColor, route.contains('H40')),
-                  buildHall(screenWidth, screenHeight, 0.43, 0.42, 'H41', hallwayColor, route.contains('H41')),
-                ],
-              );
-            },
+                  buildHall(screenWidth, screenHeight, 0.545, 0.4092, 'H39', hallwayColor, route.contains('H39')),
+                  buildHall(screenWidth, screenHeight, 0.4099, 0.4092, 'H40', hallwayColor, route.contains('H40')),
+                  buildHall(screenWidth, screenHeight, 0.4093, 0.4092, 'H41', hallwayColor, route.contains('H41')),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 
   Positioned buildRoom(
       double screenWidth, double screenHeight, double topPercent, double leftPercent, String label, Color color) {
@@ -124,16 +179,27 @@ class MapPage extends StatelessWidget {
       top: screenHeight * topPercent,
       left: screenWidth * leftPercent,
       key: Key(label),
-      child: Row(
+      child: leftPercent < 0.50 ? Row(
         children: <Widget>[
           Text(
             label,
             style: TextStyle(
-              fontSize: 14,
-              color: Colors.black,
+              fontSize: 10,
+              color: Colors.blue,
             ),
           ),
-          Icon(Icons.location_pin, color: color),
+          Icon(Icons.location_pin, color: color, size: 12,),
+        ],
+      ): Row(
+        children: <Widget>[
+          Icon(Icons.location_pin, color: color, size: 12,),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 10,
+              color: Colors.blue,
+            ),
+          ),          
         ],
       ),
     );
